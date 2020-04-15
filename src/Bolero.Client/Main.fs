7022930be710d7d77beda3765a845bfc716b4f9d
Vikram Subbaiah <vykrum@hotmail.com>
2020-04-15 23:10:05 +0530

    Host Clusters and Sub Clusters (start)

--- src/Bolero.Client/Main.fs
+++ src/Bolero.Client/Main.fs
@@ -9,16 +9,20 @@ open Microsoft.FSharp.Collections
 
 //let dd = Hexel.cls 15 [] (5.0,5.0)
 //let ff = Hexel.mcs 10 dd |> List.concat
-let cv l m = (Hexel.cls l [] (10.0,5.0)) |> Hexel.mcs m |> List.concat
+//let cv l m = (Hexel.cls l [] (15,15)) |> Hexel.mcs m |> List.concat
+//let cv l m = (Hexel.cls l (Hexel.cls m [] (20,20)) (15,15))
+let cv l m = (Hexel.cl m [] ((Hexel.cl l [] [(20,20)])|>List.concat))
 
-let x1 lst = List.map (fun x -> fst x)lst |> List.map (fun x -> x*20.0)|> List.map (fun x -> Attr("cx", x))
-let y1 lst = List.map (fun y -> snd y)lst |> List.map (fun y -> y*20.0) |> List.map (fun y -> Attr("cy", y))
+
+let x1 lst = List.map (fun x -> fst x)lst |> List.map (fun x -> x*10)|> List.map (fun x -> Attr("cx", x))
+let y1 lst = List.map (fun y -> snd y)lst |> List.map (fun y -> y*10) |> List.map (fun y -> Attr("cy", y))
 
 let cr a b = Elt("circle", [a;b;Attr("r",10);Attr("stroke","green");Attr("fill","yellow")],[])
+let cl a b = Elt("circle", [a;b;Attr("r",10);Attr("stroke","yellow");Attr("fill","green")],[])
 //let fg = List.map2 (fun x y -> cr x y) (x1 ff) (y1 ff)
 //let r = svg[Attr("width", 500);Attr("height", 500)] fg
  
-let vg (l,m) = svg[Attr("width", 500);Attr("height", 200)] (List.map2 (fun x y -> cr x y) (x1 (cv l m)) (y1 (cv l m)))
+let vg (l,m) = svg[Attr("width", 500);Attr("height", 500)] ([(List.map2 (fun x y -> cr x y) (x1 (cv l m).[0]) (y1 (cv l m).[0]));(List.map2 (fun x y -> cl x y) (x1 (cv l m).[1]) (y1 (cv l m).[1]))]|>List.concat)
 
 type Model = { 
     cll: string
@@ -26,7 +30,7 @@ type Model = {
     }
 let initModel = { 
     cll = "5"
-    hst = "10"
+    hst = "5"
     }
 
 type Message = SetCll of string| SetHst of string
