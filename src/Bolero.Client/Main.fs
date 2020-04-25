a083bac0a86ab6c80119361282db87810cba3fc1
Vikram Subbaiah <vykrum@hotmail.com>
2020-04-25 11:59:04 +0530

    20200425

--- src/Bolero.Client/Main.fs
+++ src/Bolero.Client/Main.fs
@@ -6,6 +6,48 @@ open Bolero.Html
 open Bolero.Templating.Client
 open Microsoft.FSharp.Collections
 
+//let dd = Hexel.cls 15 [] (5.0,5.0)
+//let ff = Hexel.mcs 10 dd |> List.concat
+//let cv l m = (Hexel.cls l [] (15,15)) |> Hexel.mcs m |> List.concat
+//let cv l m = (Hexel.cls l (Hexel.cls m [] (20,20)) (15,15))
+
+let cv l m =
+    (Hexel.cl m [] ((Hexel.cl l [] [(20,20)])
+    |>List.concat))
+
+//Position
+let x1 lst = 
+    List.map (fun x -> fst x)lst 
+    |> List.map (fun x -> x*10)
+    |> List.map (fun x -> Attr("cx", x))
+
+let y1 lst = 
+    List.map (fun y -> snd y)lst 
+    |> List.map (fun y -> y*10) 
+    |> List.map (fun y -> Attr("cy", y))
+
+//Geometry
+let cr a b = 
+    Elt("circle",
+        [a;b;Attr("r",10);
+        Attr("stroke","green");
+        Attr("fill","yellow")],
+        [])
+
+let cl a b = 
+    Elt("circle", [a;b;Attr("r",10);
+    Attr("stroke","yellow");
+    Attr("fill","green")],[])
+ 
+ //Render
+let vg (l,m) = 
+    svg[Attr("width", 500);Attr("height", 500)] 
+        ([(List.map2 (fun x y -> cr x y) (x1 (cv l m).[0]) (y1 (cv l m).[0]));
+        (List.map2 (fun x y -> cl x y) (x1 (cv l m).[1]) (y1 (cv l m).[1]))]
+        |>List.concat)
+
+
+
 /// Routing endpoints definition.
 type Page =
     | [<EndPoint "/">] Home
@@ -25,31 +67,15 @@ let initModel =
         hst = "5"
     }
 /// The Elmish application's update messages.
-type Message = //SetCll of string| SetHst of string
+type Message =
     | SetPage of Page | SetCll of string| SetHst of string
 
 /// Connects the routing system to the Elmish application.
-let router = Router.infer SetPage (fun model -> model.page)
+let router = 
+    Router.infer SetPage (fun model -> model.page)
 
-type Main = Template<"wwwroot/index.html">
-
-//let dd = Hexel.cls 15 [] (5.0,5.0)
-//let ff = Hexel.mcs 10 dd |> List.concat
-//let cv l m = (Hexel.cls l [] (15,15)) |> Hexel.mcs m |> List.concat
-//let cv l m = (Hexel.cls l (Hexel.cls m [] (20,20)) (15,15))
-let cv l m = (Hexel.cl m [] ((Hexel.cl l [] [(20,20)])|>List.concat))
-
-
-let x1 lst = List.map (fun x -> fst x)lst |> List.map (fun x -> x*10)|> List.map (fun x -> Attr("cx", x))
-let y1 lst = List.map (fun y -> snd y)lst |> List.map (fun y -> y*10) |> List.map (fun y -> Attr("cy", y))
-
-let cr a b = Elt("circle", [a;b;Attr("r",10);Attr("stroke","green");Attr("fill","yellow")],[])
-let cl a b = Elt("circle", [a;b;Attr("r",10);Attr("stroke","yellow");Attr("fill","green")],[])
-//let fg = List.map2 (fun x y -> cr x y) (x1 ff) (y1 ff)
-//let r = svg[Attr("width", 500);Attr("height", 500)] fg
- 
-let vg (l,m) = svg[Attr("width", 500);Attr("height", 500)] ([(List.map2 (fun x y -> cr x y) (x1 (cv l m).[0]) (y1 (cv l m).[0]));(List.map2 (fun x y -> cl x y) (x1 (cv l m).[1]) (y1 (cv l m).[1]))]|>List.concat)
-
+type Main =
+    Template<"wwwroot/index.html">
 
 let update message model =
     match message with
@@ -86,4 +112,3 @@ type MyApp() =
 
     override this.Program =
         Program.mkSimple (fun _ -> initModel) update view
-        //|> Program.withRouter router
